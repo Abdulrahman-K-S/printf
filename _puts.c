ecf77a5b75b5e4d78a8f4bb48482bdfd993c7815
fadirafik <fadihadddad11@gmail.com>
2023-08-21 03:12:55 +0300

    added _puts.c file

---
+++ _puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @s: string input
+ *
+ * Return: always 0
+*/
+int _puts(char *s)
+{
+    int i = 0;
+    while(s[i] != '\0')
+    {
+        _putchar(s[i]);
+        i++;
+    }
+    _putchar('\0');
+    return(0);
+}
