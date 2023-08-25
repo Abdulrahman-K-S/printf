0a0fa2f39f47baf812e60d361fe9826ba1496964
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-25 15:05:04 -0700

    Reverted back

--- _putchar.c
+++ _putchar.c
@@ -10,15 +10,5 @@
 
 int _putchar(char c)
 {
-	static int i;
-	static char buf[OUT_BUF_S];
-
-	if (c == BF_FLUSH || i >= OUT_BUF_S)
-	{
-		write(1, buf, i);
-		i = 0;
-	}
-	if (c != BF_FLUSH)
-		buf[i++] = c;
-	return (1);
+	return (write(1, &c, 1));
 }
--- main.h
+++ main.h
@@ -6,9 +6,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-#define OUT_BUF_S 1024
-#define BF_FLUSH -1
-
 /**
  * struct specifiers - A specifier struct.
  *
