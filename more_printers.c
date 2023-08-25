f4d8ca965a2cd9d34fed9bc1b02391ab5fc2165f
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-25 05:45:10 -0700

    Fixed the less than 0 problem

--- more_printers.c
+++ more_printers.c
@@ -1,53 +1,6 @@
 #include "main.h"
 
 /**
- * _print_unsigned - A function to print the unsigned integer.
- *
- * @args: Argument pointer
- *
- * Return: Length of the printed characters.
- */
-int _print_unsigned(va_list args)
-{
-	int length = 1;
-	unsigned int arg, temp;
-
-	arg = va_arg(args, unsigned int);
-	temp = arg;
-
-	if (temp < 0)
-	{
-		_putchar('-');
-		temp *= -1;
-		arg = temp;
-		length++;
-	}
-	while (arg > 9)
-	{
-		arg /= 10;
-		length++;
-	}
-
-	_unsigned_recursion(temp);
-	return (length);
-}
-
-/**
- * _unsigned_recursion - A function that uses recursion to print out
- *                       the unsigned integer.
- *
- * @integer: The integer to be printed.
- */
-void _unsigned_recursion(int integer)
-{
-	unsigned int t = integer;
-
-	if (t / 10)
-		_unsigned_recursion(t / 10);
-	_putchar(t % 10 + '0');
-}
-
-/**
  * _print_binary - takes a decimal number and prints a binary one
  * @args: argument pointer
  *
@@ -96,4 +49,44 @@ void _long_recursion(long binary)
 	if (binary / 10)
 		_long_recursion(binary / 10);
 	_putchar(binary % 10 + '0');
+}
+
+/**
+ * _print_unsigned - A function to print the unsigned integer.
+ *
+ * @args: Argument pointer
+ *
+ * Return: Length of the printed characters.
+ */
+int _print_unsigned(va_list args)
+{
+	int length = 1;
+	unsigned int arg, temp;
+
+	arg = va_arg(args, unsigned int);
+	temp = arg;
+
+	while (arg > 9)
+	{
+		arg /= 10;
+		length++;
+	}
+
+	_unsigned_recursion(temp);
+	return (length);
+}
+
+/**
+ * _unsigned_recursion - A function that uses recursion to print out
+ *                       the unsigned integer.
+ *
+ * @integer: The integer to be printed.
+ */
+void _unsigned_recursion(int integer)
+{
+	unsigned int t = integer;
+
+	if (t / 10)
+		_unsigned_recursion(t / 10);
+	_putchar(t % 10 + '0');
 }
