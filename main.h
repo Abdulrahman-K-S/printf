7f54bc9b134d3d795ce9cdd9a4f0f758b052fc0d
Abdulrahman Khaled <21-101079@students.eui.edu.eg>
2023-08-25 04:43:00 -0700

    Added the struct and changed the binary function name

--- main.h
+++ main.h
@@ -6,6 +6,18 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct specifiers - A specifier struct.
+ *
+ * @specifier: A character to indicate which specifier this is.
+ * @f: Pointer to a it's respective print function.
+ */
+typedef struct specifiers
+{
+	char *specifier;
+	int (*f)(va_list args);
+} spec_t;
+
 /* Our printers */
 int _putchar(char c);
 int _puts(char *s);
@@ -15,9 +27,9 @@ int _puts(char *s);
 int _print_char(va_list args);
 int _print_string(va_list args);
 int _print_int(va_list args);
+int _print_binary(va_list args);
 void _integer_recursion(int integer);
 void _long_recursion(long binary);
-int dec_binary(va_list args);
 long recursion_binary(long b);
 
 /* print_format.c module */
